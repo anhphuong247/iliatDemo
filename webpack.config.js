20038bb2d51baee4558ed8ff719e794863bb30ef
anhphuong247 <phuongit247@gmail.com>
2016-08-02 11:46:53 -0700

    up

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,18 @@ module.exports = {
         }
     ]
   },
-   
+    proxy: {
+      '/some/path*': {
+        target: 'https://other-server.example.com',
+        secure: false,
+          bypass: function(req, res, proxyOptions) {
+            if (req.headers.accept.indexOf('html') !== -1) {
+            console.log('Skipping proxy for browser request.');
+            return '/index.html';
+            }
+          }
+      }
+    },
   output: {
     path: __dirname + "/src/",
     filename: "client.min.js"
